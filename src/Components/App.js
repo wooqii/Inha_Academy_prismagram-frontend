ad73cbcb75c213ea94b5ea054ea232b1aa8962dd
wooqii <cpawookie@gmail.com>
2020-02-21 14:23:50 +0900

    200221_Front_1
    Global_Style & Theme

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyles from '../Styles/GlobalStyles';
+import { ThemeProvider } from "styled-components";
+import Theme from "../Styles/Theme"
+
+export default () => ( 
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    hello
+  </ThemeProvider>
+);
